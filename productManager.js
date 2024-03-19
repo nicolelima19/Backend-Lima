d800cdcd4b0a15b91799f28140c20112263a4def
Nicole <chesterfxx@gmail.com>
2024-03-19 12:48:18 -0300

    Segunda pre entrega Manjo de Archivos

--- productManager.js
+++ productManager.js
@@ -1,22 +1,57 @@
+const fs = require('fs');
+
 class ProductManager {
     #products;
+    #path;
     static idProducto = 0;
 
     constructor() {
-        this.#products = [];
+        this.#path = './data/productos.json';
+        this.#products = this.#obtenerProductos();
+    }
+
+    #asignarId() {
+        let id = 1;
+        if (this.#products.length !== 0) {
+            id = this.#products[this.#products.length - 1].id + 1;
+        }
+        return id;
+    }
+
+    #obtenerProductos() {
+        try {
+            if (fs.existsSync(this.#path)) {
+                const data = fs.readFileSync(this.#path, 'utf-8');
+                return JSON.parse(data);
+            }
+            return [];
+        } catch (error) {
+            console.error('Ocurrió un error al obtener el archivo de los productos:', error);
+            return [];
+        }
+    }
+
+    #guardarArchivo() {
+        try {
+            fs.writeFileSync(this.#path, JSON.stringify(this.#products));
+            console.log('Archivo de productos guardado exitosamente.');
+        } catch (error) {
+            console.error('Ocurrió un error al guardar el archivo de los productos:', error);
+        }
     }
 
     addProduct(title, description, price, thumbnail, code, stock) {
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            return 'Todos los parametros son requeridos [title, description, price, thumbnail, code, stock]';
+        }
 
-        if(!title || !description || !price || !thumbnail || !code || !stock)
-        return 'Todos los parametros son requeridos [title, description, price, thumbnail, code, stock]'
-
-        const codeRepetido = this.#products.some(producto => producto.code == code)
-        if(codeRepetido)
-        return 'El codigo ${code} ya se encuentra registrado en otro producto'
+        const codeRepetido = this.#products.some(producto => producto.code === code);
+        if (codeRepetido) {
+            return `El código ${code} ya se encuentra registrado en otro producto`;
+        }
 
         ProductManager.idProducto = ProductManager.idProducto + 1;
-        const id = ProductManager.idProducto;
+        const id = this.#asignarId();
 
         const nuevoProducto = {
             id,
@@ -28,6 +63,7 @@ class ProductManager {
             stock,
         };
         this.#products.push(nuevoProducto);
+        this.#guardarArchivo();
 
         return 'Producto agregado correctamente';
     }
@@ -37,13 +73,41 @@ class ProductManager {
     }
 
     getProductById(id) {
-        this.#products.find(producto => producto.id == id)
-        if (producto)
+        const producto = this.#products.find(producto => producto.id == id);
+        if (producto) {
             return producto;
-        else
-            return 'Not found del producto con id ${id}';
+        } else {
+            return `Not found del producto con id ${id}`;
+        }
+    }
+
+    updateProduct(id, updateObjets) {
+        let mensaje = `El producto ${id} no existe`;
+
+        const index = this.#products.findIndex(p => p.id === id);
+
+        if (index !== -1) {
+            const { id, ...rest } = updateObjets;
+            this.#products[index] = { ...this.#products[index], ...rest };
+            this.#guardarArchivo();
+            mensaje = 'Producto actualizado';
+        }
+
+        return mensaje;
+    }
+
+    deleteProduct(id) {
+        let mensaje = `El producto con ${id} no existe`;
+
+        const index = this.#products.findIndex(p => p.id === id);
+        if (index !== -1) {
+            this.#products = this.#products.filter(p => p.id !== id);
+            this.#guardarArchivo();
+            mensaje = 'Producto eliminado';
+        }
+
+        return mensaje;
     }
 }
-
 
 module.exports = ProductManager;
--- testing.js
+++ testing.js
@@ -1,10 +1,21 @@
-const ProductManager = require("./productManager");
+const ProductManager = require ("./productManager.js");
 
 const producto = new ProductManager();
 
-/* console.log = producto.getProducts();
-console.log(producto.getProductsById(1)); */
+// DEJO COMENTADO LO PEDIDO EN EL DESAFIO PARA PROBAR SI ES CORRECTO. 
 
-console.log(producto.addProduct('Consola PS5', 'PlayStation 5', 1200, 'https://f.fcdn.app/imgs/0aad68/www.covercompany.com.uy/coveuy/ebbc/original/catalogo/2-4549_11529_1/1500-1500/consola-sony-playstation-5-slim-digital-edition-1tb-ps5-blanco.jpg', '28897', '25'));
+/* console.log(producto.addProduct('Consola PS5', 'PlayStation 5', 1200, 'https://f.fcdn.app/imgs/0aad68/www.covercompany.com.uy/coveuy/ebbc/original/catalogo/2-4549_11529_1/1500-1500/consola-sony-playstation-5-slim-digital-edition-1tb-ps5-blanco.jpg', '2889', '25'));
+console.log(producto.addProduct('Nintendo Switch Oled', 'Nintendo Switch Oled', 750, 'https://assets.nintendo.com/image/upload/f_auto/q_auto/dpr_2.0/c_scale,w_300/ncom/en_US/switch/site-design-update/HEGS_001_heroWA_07_R_ad-0-OLED-V4', '6352', '30'));
+console.log(producto.addProduct('Laptop PREDATOR', 'Predator', 2500, 'https://netpc.uy/wp-content/uploads/2021/11/3-21.png', '3242', '5'));
+console.log(producto.addProduct('iPad 10th generación', 'Apple iPad', 990, 'https://m.media-amazon.com/images/I/61uA2UVnYWL._AC_UF894,1000_QL80_.jpg', '7644', '15')); */
 
-console.log(producto.getProducts());
+/* console.log(producto.getProducts()); */
+
+/* console.log(producto.deleteProduct(4)); */
+
+/* const productoActualizar = {
+        "price": 890,
+        "stock": "10"
+}
+
+console.log(producto.updateProduct(4, productoActualizar)); */
