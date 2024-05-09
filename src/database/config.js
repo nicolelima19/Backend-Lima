5b659555fa5754d58ee56d0506093c2994c2298d
Nicole <chesterfxx@gmail.com>
2024-05-09 11:26:37 -0300

    Correcion de error

--- src/database/config.js
+++ src/database/config.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = async () => {
     try{
-        await mongoose.connect(process.env.URI_MONGO_DB);
+        await mongoose.connect(process.env.URI_MONGO_DB, {dbName:process.env.NAME_DB});
         console.log('Base de datos online');
     } catch (error) {
         console.log(`Error al levantar la base de datos ${error}`);
